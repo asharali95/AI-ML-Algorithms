70a3a209fee55d1c02e95cbfb380bd7c66252d0a
Ashar Ali <ashar.alia1999@gmail.com>
2021-04-22 01:07:36 +0500

    Modification in A-star search algorithm

--- Informed Search Technique/A steric Search.py
+++ Informed Search Technique/A steric Search.py
@@ -1,24 +1,53 @@
-dictree={'A':['B','E','C'],'B':['E','D'],'C':['F','G'],'D':[], 'E':[],'F':[],'G':[] }
-roottree=list(dictree.keys())
-openlist=['A']
+'''graph={'s':['a','b'],'a':['b','c','d'],'b':['c','d'],'c':['d'],'d':[] }
+pathcost = {'sa':3,'sb':2,'ab':3,'ac':1,'ad':3,'bc':5,'bd':3,'cd':2}
+hcost = {'s':1, 'a':3, 'b':3, 'c':0, 'd':0}
+start=input("Enter start state:");
+goal = input("Enter goal state: ");
+ls = [start]
+lscost = [hcost[start]]
+print(lscost)
 closelist=[]
-goalstate='G'
-traversalturn=1
-print(f"\n------{str(traversalturn)}-----\n")
-print(f"openlist: {openlist} \t\t\t\t\t closelist:{closelist}")
-for vertextree in roottree:
-    closelist.append(openlist.pop(0))
-    traversalturn+=1
-    print(f"\n------{str(traversalturn)}-----")
-    if (closelist[-1]!=goalstate):
-	    i=dictree[closelist[-1]]
-	    for neighbours in i:
-		    if(neighbours not in openlist):
-		        openlist.append(neighbours)
-	    print(f"openlist: {openlist} \t\t\t\t\t closelist:{closelist}")
-    else:
-        print(f"openlist: {openlist} \t\t\t\t\t closelist:{closelist}")
+print(ls,"\t\t\t\t closelist")
+while len(ls)>0:
+    min_index = lscost.index(min(lscost))
+    cost = lscost.pop(min_index)
+    n = ls.pop(min_index)
+    cn = n[len(n)-1]
+    if cn == goal:
+        closelist.append(n)
         break
-print("\n\n path:",end="-")
-for i in closelist:
-    print(i,end="-")
+    nodes= graph[cn]
+    nmin = pathcost[cn+nodes[0]]+cost
+    for i in nodes:
+        ls.append(n+i)
+        lscost.append(cost+pathcost[cn+i]+hcost[i]-hcost[cn])
+    closelist.append(n)
+    print(ls,"\t\t\t",closelist)
+print(ls,"\t\t\t",closelist)'''
+#---------------------------------------------------------------
+graph={'S':['A','B'],'A':['G'],'B':['G'],'G':[], }
+pathcost = {'SA':2,'SB':2,'AG':2,'BG':2}
+hcost = {'S':3, 'A':2, 'B':1, 'G':0}
+start=input("Enter start state:");
+goal = input("Enter goal state: ");
+ls = [start]
+lscost = [hcost[start]]
+print(lscost)
+closelist=[]
+
+while len(ls)>0:
+    min_index = lscost.index(min(lscost))
+    cost = lscost.pop(min_index)
+    n = ls.pop(min_index)
+    cn = n[len(n)-1]
+    if cn == goal:
+        closelist.append(n)
+        break
+    nodes= graph[cn]
+    nmin = pathcost[cn+nodes[0]]+cost
+    for i in nodes:
+        ls.append(n+i)
+        lscost.append(cost+pathcost[cn+i]+hcost[i]-hcost[cn])
+    closelist.append(n)
+    print(ls,"\t\t\t",closelist)
+print(ls,"\t\t\t",closelist)
