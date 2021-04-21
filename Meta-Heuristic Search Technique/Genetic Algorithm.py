e9ca930372db0de5fffe69ab1d86bb5f86e5f972
Ashar Ali <ashar.alia1999@gmail.com>
2021-04-21 23:05:13 +0500

    Few logical error correction

--- Meta-Heuristic Search Technique/Genetic Algorithm.py
+++ Meta-Heuristic Search Technique/Genetic Algorithm.py
@@ -1,12 +1,12 @@
-noOfGenes = eval(input("Enter number of genes: "))
-noOfChromosomes = eval(input("Enter no of chromosomes: "))
+noOfChromosomes = eval(input("Enter number of chromosomes: "))
+noOfGenes = eval(input("Enter no of genes: "))
 index = 0
 S=[]
 tempList = []
 
-for gene in range(noOfGenes):
-    print("Enter chromosomes of gene",index+1,"(must be 0 and 1 only): ")
-    for chromosomes in range(noOfChromosomes):
+for choromosome in range(noOfChromosomes):
+    print("Enter genes of chromosome",index+1,"(must be 0 and 1 only): ")
+    for gene in range(noOfGenes):
         tempList.append(eval(input()))
     S.append(tempList)
     print(S)
@@ -30,15 +30,16 @@ for i in range(len(desc)):
         if desc[i].count(1)>desc[j].count(1):
            desc[j],desc[i]=desc[i],desc[j]
 print("s =" , desc)
-print("Cross over after 2 points")
-for i in range(4):
+print("==Cross over==")
+crossOver = eval(input("Crossover after which point? "))
+for i in range(crossOver,noOfGenes):
     desc[1][i],desc[2][i]=desc[2][i],desc[1][i]
     desc[1],desc[2]=desc[2],desc[1]
 print("s2 and s3")
 print("s2=", desc[1], "s3=", desc[2])
-print("s=", desc)
-print("Mutation")
-for i in range(6):
+
+print("==Mutation==")
+for i in range(noOfChromosomes):
     for j in range(2):
         if(desc[i][j]==0):
             desc[i][j]=1
@@ -54,3 +55,4 @@ else:
     print("Old Fitness is greater before applying Genetic Algorithm")
     print("old fitness: ",fit)
     print("updated fitness: ",fit2)
+
